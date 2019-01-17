d8d05c96db327dd17fff2f8b3eae62048dad27f3
dadamsretrorabbit <dadams@retrorabbit.co.za>
2019-01-17 10:45:51 +0200

    Adds Square Method

---
+++ ShapeArea/ShapeArea/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeArea
+{
+    class Square
+    {
+
+        private double length;
+        public void getArea()
+        {
+            Console.WriteLine("Provide the length for your square:");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your area of square is : " + (Math.Pow(length, 2)));
+        }
+
+    }
+}
